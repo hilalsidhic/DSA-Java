c98054fe0fd1e9ceba99a4fe4869e748c815e848
hilalsidhic <hilalsidhic21@gmail.com>
2022-06-22 11:52:48 +0530

    fib formula

--- program/Recursion/src/com/hilal/Fibonacci.java
+++ program/Recursion/src/com/hilal/Fibonacci.java
@@ -2,12 +2,15 @@ package com.hilal;
 
 public class Fibonacci {
     public static void main(String[] args) {
-        System.out.println(fib(7));
+        System.out.println(fibformula(50));
+    }
+    public static int fibformula(int num){
+        return (int)((Math.pow((1+Math.sqrt(5))/2,num) - Math.pow((1-Math.sqrt(5))/2,num))/Math.sqrt(5));
     }
     public static int fib(int num){
-        if(num==1)
+        if(num==0)
             return 0;
-        if(num==2)
+        if(num==1)
             return 1;
         return fib(num-1)+fib(num-2);
     }
