21398b90186df76bd4366fc3270e812de11eb710
hilalsidhic <hilalsidhic21@gmail.com>
2022-06-28 17:14:45 +0530

    subset ascii subsequence

---
+++ program/Recursion/src/Subsets/Ascii.java
@@ -0,0 +1,26 @@
+package Subsets;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Ascii {
+    public static void main(String[] args) {
+        ArrayList<String> list = sub("","ab");
+        System.out.println(Arrays.toString(list.toArray()));
+    }
+    public static ArrayList<String> sub(String p,String up){
+        ArrayList<String> list = new ArrayList<>();
+        if(up.isEmpty()){
+            list.add(p);
+            return list;
+        }
+        char ch = up.charAt(0);
+        ArrayList<String> newlist=sub(p+ch,up.substring(1));
+        list.addAll(newlist);
+        ArrayList<String> newlist1=sub(p+(int)(ch),up.substring(1));
+        list.addAll(newlist1);
+        ArrayList<String> newlist2=sub(p,up.substring(1));
+        list.addAll(newlist2);
+        return list;
+    }
+}
---
+++ program/Recursion/src/Subsets/Subsequence.java
@@ -0,0 +1,24 @@
+package Subsets;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Subsequence {
+    public static void main(String[] args) {
+        ArrayList<String> list = sub("","abc");
+        System.out.println(Arrays.toString(list.toArray()));
+    }
+    public static ArrayList<String> sub(String p, String up){
+        ArrayList<String> list = new ArrayList<>();
+        if(up.isEmpty()){
+            list.add(p);
+            return list;
+        }
+        char ch = up.charAt(0);
+        ArrayList<String> newlist=sub(p+ch,up.substring(1));
+        list.addAll(newlist);
+        ArrayList<String> newlist2=sub(p,up.substring(1));
+        list.addAll(newlist2);
+        return list;
+    }
+}
